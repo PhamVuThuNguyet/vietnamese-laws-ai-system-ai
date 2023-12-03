19600c21e913f23656211329d24c642a8c723e06
PhamVuThuNguyet <pvtnguyet.19it1vku@gmail.com>
2023-12-03 20:38:09 +0700

    Finish final code structure for AI System

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from dotenv import load_dotenv
+import langchain
 
-from schema import TrainingRequest, ChatRequest, SearchRequest, BatchTrainingRequest
-import data_training
-from chat import ChatAgent
+from core.data_ingestor import DataIngestor
+from schemas import BatchTrainingRequest, ChatRequest, SearchRequest, TrainingRequest
+from routes.chat import chat as ChatAgent
+from llm.base_model.langchain_openai import LangchainOpenAI
+import os 
+import logging
 
-app = FastAPI()
+logging.basicConfig(level=logging.INFO)
+
+app = FastAPI(title="Vietnamese Laws Exploration", version="1.0.0")
 
 # Set up CORS
 origins = ["*"]
@@ -21,7 +27,6 @@ app.add_middleware(
 # Load environment variables
 load_dotenv()
 
-
 @app.get("/health")
 async def pong():
     return {"ping": "pong!"}
@@ -30,24 +35,27 @@ async def pong():
 @app.post("/train-data")
 async def train_data(request: TrainingRequest):
     charter = request.charter
-    data_training.ingest_plain_text(charter)
+    data_ingestor = DataIngestor()
+    data_ingestor.load_charter(charter)
+
 
 @app.post("/train-data-batch")
 async def train_data(request: BatchTrainingRequest):
     list_of_charter = request.charter_list
     for charter in list_of_charter:
         new_request = TrainingRequest(charter=charter)
-        await train_data(new_request)
+        await train_data(request=new_request)
+
 
 @app.post("/chat")
 async def chat(request: ChatRequest):
-    messages = request.messages
-    question = messages[-1]
-    chat_agent = ChatAgent()
-    relevant_docs =  chat_agent.query_relevant_docs(question)
-    response = chat_agent.chat(question, relevant_docs)
+    response = await ChatAgent(request)
     return response
+
 
 @app.post("/search")
 async def search(request: SearchRequest):
-    pass
+    keyword = request.keyword
+    _, retriever = LangchainOpenAI.get_langchain_retriever()
+    result = await retriever._aget_relevant_documents(keyword)
+    return result
---
+++ llm/data_loader/vectorstore_retriever.py
@@ -0,0 +1,111 @@
+import asyncio
+from functools import partial
+from typing import Any, ClassVar, Collection, Dict, List
+
+from langchain.callbacks.manager import (
+    AsyncCallbackManagerForRetrieverRun,
+    CallbackManagerForRetrieverRun,
+)
+from langchain.schema import BaseRetriever, Document
+from langchain.vectorstores import VectorStore
+from pydantic import Field, root_validator
+
+
+class CustomVectorStoreRetriever(BaseRetriever):
+    """Retriever class for VectorStore."""
+
+    vectorstore: VectorStore
+    """VectorStore to use for retrieval."""
+    search_type: str = "similarity"
+    """Type of search to perform. Defaults to "similarity"."""
+    search_kwargs: dict = Field(default_factory=dict)
+    """Keyword arguments to pass to the search function."""
+    allowed_search_types: ClassVar[Collection[str]] = (
+        "similarity",
+        "similarity_score_threshold",
+        "mmr",
+    )
+
+    class Config:
+        """Configuration for this pydantic object."""
+
+        arbitrary_types_allowed = True
+
+    @root_validator()
+    def validate_search_type(cls, values: Dict) -> Dict:
+        """Validate search type."""
+        search_type = values["search_type"]
+        if search_type not in cls.allowed_search_types:
+            raise ValueError(
+                f"search_type of {search_type} not allowed. Valid values are: "
+                f"{cls.allowed_search_types}"
+            )
+        if search_type == "similarity_score_threshold":
+            score_threshold = values["search_kwargs"].get("score_threshold")
+            if (score_threshold is None) or (not isinstance(score_threshold, float)):
+                raise ValueError(
+                    "`score_threshold` is not specified with a float value(0~1) "
+                    "in `search_kwargs`."
+                )
+        return values
+
+    def _get_relevant_documents(
+        self, query: str, *, run_manager: CallbackManagerForRetrieverRun
+    ) -> List:
+        if self.search_type == "similarity":
+            docs_with_scores = self.vectorstore.similarity_search_with_score(
+                query, **self.search_kwargs
+            )
+        elif self.search_type == "similarity_score_threshold":
+            docs_with_scores = self.vectorstore.similarity_search_with_relevance_scores(
+                query, **self.search_kwargs
+            )
+
+        elif self.search_type == "mmr":
+            embedding = self.vectorstore.embed_query(query)
+            docs_with_scores = (
+                self.vectorstore.max_marginal_relevance_search_with_score_by_vector(
+                    embedding, **self.search_kwargs
+                )
+            )
+        else:
+            raise ValueError(f"search_type of {self.search_type} not allowed.")
+
+        return docs_with_scores
+
+    async def _aget_relevant_documents(
+        self, query: str, *, run_manager: AsyncCallbackManagerForRetrieverRun
+    ) -> List:
+        if self.search_type == "similarity":
+            func = partial(
+                self.vectorstore.similarity_search_with_score,
+                query,
+                **self.search_kwargs,
+            )
+            docs_with_scores = await asyncio.get_event_loop().run_in_executor(
+                None, func
+            )
+        elif self.search_type == "similarity_score_threshold":
+            func = partial(
+                self.vectorstore.similarity_search_with_relevance_scores,
+                query,
+                **self.search_kwargs,
+            )
+
+            docs_with_scores = await asyncio.get_event_loop().run_in_executor(
+                None, func
+            )
+        elif self.search_type == "mmr":
+            embedding = self.vectorstore.embedding_function(query)
+            func = partial(
+                self.vectorstore.max_marginal_relevance_search_with_score_by_vector,
+                embedding,
+                **self.search_kwargs,
+            )
+            docs_with_scores = await asyncio.get_event_loop().run_in_executor(
+                None, func
+            )
+        else:
+            raise ValueError(f"search_type of {self.search_type} not allowed.")
+
+        return docs_with_scores
